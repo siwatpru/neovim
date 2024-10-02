8f798f11d82ad59efb5610e87b584ff127bf673e
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-10-02 22:14:02 +0700

    Change background to transparent

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,6 +35,9 @@ require("lazy").setup({
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
+  ui = {
+    backdrop = 100,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
