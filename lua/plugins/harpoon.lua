637030474aaf7436a7d1ee69074c95914ab6806c
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-10-18 16:28:43 +0700

    Change harpoon binding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,15 +7,17 @@ return {
     { "<leader>3", false },
     { "<leader>4", false },
     { "<leader>5", false },
+    { "<leader>h", false },
+    { "<leader>H", false },
     {
-      "<leader>h",
+      "<leader>a",
       function()
         require("harpoon"):list():add()
       end,
       desc = "Harpoon File",
     },
     {
-      "<leader>H",
+      "<leader>o",
       function()
         local harpoon = require("harpoon")
         harpoon.ui:toggle_quick_menu(harpoon:list())
@@ -23,32 +25,39 @@ return {
       desc = "Harpoon Quick Menu",
     },
     {
-      "<leader>j",
+      "<leader>h",
       function()
         require("harpoon"):list():select(1)
       end,
       desc = "Harpoon to File 1",
     },
     {
-      "<leader>k",
+      "<leader>j",
       function()
         require("harpoon"):list():select(2)
       end,
       desc = "Harpoon to File 2",
     },
     {
-      "<leader>l",
+      "<leader>k",
       function()
         require("harpoon"):list():select(3)
       end,
       desc = "Harpoon to File 3",
     },
     {
-      "<leader>;",
+      "<leader>l",
       function()
         require("harpoon"):list():select(4)
       end,
       desc = "Harpoon to File 4",
+    },
+    {
+      "<leader>;",
+      function()
+        require("harpoon"):list():select(5)
+      end,
+      desc = "Harpoon to File 5",
     },
   },
 }
