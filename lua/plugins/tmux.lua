2373943bb30e52e481bfbfedddd9d5c18aefe039
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-10-28 11:04:51 +0700

    Change navigation key to work in vim and tmux

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,36 +1,54 @@
 return {
-  -- Other plugin configurations
   {
-    "LazyVim/LazyVim", -- assuming LazyVim itself is included here
+    "LazyVim/LazyVim",
     keys = {
-      -- Use tmux commands for pane navigation if in a tmux session
       {
         "<C-h>",
         function()
-          require("custom.tmux_nav").move("h")
+          -- Check if a left split exists
+          if vim.fn.winnr() ~= vim.fn.winnr("h") then
+            vim.cmd("wincmd h")
+          elseif vim.fn.exists("$TMUX") == 1 then
+            require("custom.tmux_nav").move("h")
+          end
         end,
-        desc = "Move left between tmux and vim",
+        desc = "Move left between vim and tmux",
       },
       {
         "<C-j>",
         function()
-          require("custom.tmux_nav").move("j")
+          -- Check if a down split exists
+          if vim.fn.winnr() ~= vim.fn.winnr("j") then
+            vim.cmd("wincmd j")
+          elseif vim.fn.exists("$TMUX") == 1 then
+            require("custom.tmux_nav").move("j")
+          end
         end,
-        desc = "Move down between tmux and vim",
+        desc = "Move down between vim and tmux",
       },
       {
         "<C-k>",
         function()
-          require("custom.tmux_nav").move("k")
+          -- Check if an up split exists
+          if vim.fn.winnr() ~= vim.fn.winnr("k") then
+            vim.cmd("wincmd k")
+          elseif vim.fn.exists("$TMUX") == 1 then
+            require("custom.tmux_nav").move("k")
+          end
         end,
-        desc = "Move up between tmux and vim",
+        desc = "Move up between vim and tmux",
       },
       {
         "<C-l>",
         function()
-          require("custom.tmux_nav").move("l")
+          -- Check if a right split exists
+          if vim.fn.winnr() ~= vim.fn.winnr("l") then
+            vim.cmd("wincmd l")
+          elseif vim.fn.exists("$TMUX") == 1 then
+            require("custom.tmux_nav").move("l")
+          end
         end,
-        desc = "Move right between tmux and vim",
+        desc = "Move right between vim and tmux",
       },
     },
   },
