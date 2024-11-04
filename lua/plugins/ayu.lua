688e864ba18982db375c03fb0fe42f72cdd5cc17
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-11-04 15:25:19 +0700

    Change ayu setting

--- lua/plugins/ayu.lua
+++ lua/plugins/ayu.lua
@@ -3,21 +3,8 @@ return {
     "Shatur/neovim-ayu",
     config = function()
       require("ayu").setup({
-        mirage = true,
-        terminal = true,
-        overrides = {
-          Normal = { bg = "None" },
-          ColorColumn = { bg = "None" },
-          SignColumn = { bg = "None" },
-          Folded = { bg = "None" },
-          FoldColumn = { bg = "None" },
-          CursorLine = { bg = "None" },
-          CursorColumn = { bg = "None" },
-          WhichKeyFloat = { bg = "None" },
-          VertSplit = { bg = "None" },
-          FloatBorder = { bg = "None" },
-          NormalFloat = { bg = "None" },
-        },
+        mirage = false,
+        terminal = false,
       })
     end,
   },
