ac6451cb44a395c4edf785c8ad9d2edc58a284e6
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-11-22 00:48:29 +0700

    Fix transparent for noice

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,24 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = true,
     opts = {
-      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      views = {
+        mini = {
+          win_options = {
+            winblend = 0,
+          },
+        },
+      },
     },
   },
   -- {
